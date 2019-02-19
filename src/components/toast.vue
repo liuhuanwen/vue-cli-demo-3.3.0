00eb297c76cd3b4719a79731568cc102bb5a91fe
liuhw <liuhuansir@126.com>
2019-02-19 17:55:47 +0800

    完成toast

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -9,7 +9,7 @@
     name: "toast",
     data() {
       return {
-        message: '112312312312311111111111'
+        message: '请输入手机号码请输入手机号请输入手机号码请输'
       }
     }
   }
@@ -18,10 +18,15 @@
 <style lang="scss" scoped>
   .message {
     position: fixed;
-    bottom: 300px;
-    width: 400px;
+    bottom: 250px;
+    max-width: 400px;
     left: 50%;
     transform: translateX(-50%);
     z-index: 1000;
+    word-break: break-all;
+    background-color: #333;;
+    border-radius: 40px;
+    padding: 24px;
+    color: #fff;
   }
 </style>
