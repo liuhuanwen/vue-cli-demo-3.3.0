f8e723321218bb6173d96113411b2b970461194d
liuhw <liuhuansir@126.com>
2019-02-20 11:51:10 +0800

    有关我们页面

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <title-bar title="关于我们"></title-bar>
+    <div class="about">
+      <h3>公司</h3>
+      <p>上海拉扎斯信息科技有限公司</p>
+      <h3>地址</h3>
+      <p>上海市普陀区金沙江路1518弄近铁城市广场401室</p>
+      <h3>电话</h3>
+      <p>(021) 8024-1717</p>
+    </div>
   </div>
 </template>
 
@@ -14,3 +22,21 @@
   }
 
 </script>
+
+<style lang="scss" scoped>
+  .about {
+    text-align: left;
+    margin-left: 32px;
+    h3 {
+      font-size: 30px;
+      margin-top: 40px;
+      color: #333;
+    }
+    p {
+      font-size: 28px;
+      margin-top: 8px;
+      color: #666;
+    }
+  }
+
+</style>
